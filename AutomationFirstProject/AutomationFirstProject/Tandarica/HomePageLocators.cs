862cc8ca2dc7c48a0097c4e23aacd8c40b68a399
IulicaEnachescu <iulica.enachescu@gmail.com>
2020-10-28 19:38:11 +0200

    m

--- AutomationFirstProject/AutomationFirstProject/Tandarica/HomePageLocators.cs
+++ AutomationFirstProject/AutomationFirstProject/Tandarica/HomePageLocators.cs
@@ -22,7 +22,6 @@ namespace AutomationFirstProject.Tandarica
 
         private IWebElement teamButton => _driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='menu-item-2914']/a/span[2]")));
         private IWebElement actorButton => _driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='av-masonry-1-item-2697']/figure/div/div")));
-        private IWebElement actorImageButton => _driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='av-masonry-1-item-2697']/figure/div/div/img")));
         private IWebElement showButton => _driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='av-layout-grid-1']/div[1]/div/div[1]/section/div/p[2]")));
         public IWebElement showNameText => _driverWait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='av-layout-grid-1']/div[2]/div/div/h3")));
 
